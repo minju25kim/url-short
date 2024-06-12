92be44531ac25bd703d794c5f9fd6329649521ac
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-06-12 18:28:49 +0900

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const dns = require("dns");
+const express = require("express");
+const cors = require("cors");
+// const bodyParser = require("body-parser");
 const app = express();
+const reset = "\x1b[0m";
+const blue = "\x1b[34m";
+
+let urlCounter = 1;
+
+const urlMappings = new Map();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.post("/api/shorturl", function (req, res) {
+  dns.lookup(new URL(req.body.url).hostname, (err, address) => {
+    if (err) {
+      console.error("Error:", err);
+      res.json({ error: "invalid url" });
+    } else {
+      console.log("Resolved IP address:", address);
+      const shortUrl = urlCounter++;
+      urlMappings.set(shortUrl, req.body.url);
+      res.json({ original_url: req.body.url, short_url: shortUrl });
+    }
+  });
+});
+
+app.get("/api/shorturl/:number", function (req, res) {
+  const shortUrl = parseInt(req.params.number);
+  if (urlMappings.has(shortUrl)) {
+    const originalUrl = urlMappings.get(shortUrl);
+    res.redirect(originalUrl);
+  } else {
+    // Short URL not found
+    res.status(404).send("Short URL not found");
+  }
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Your app is listening on port " +
+      blue +
+      "http://localhost:" +
+      listener.address().port +
+      "/" +
+      reset
+  );
 });
